fd5197402b0a16381241d7bf2a12ba551a4a3802
grubnubble <montanalisagoodman@gmail.com>
2019-08-14 10:45:36 -0400

    Successfully post to safeharbor endpoint

--- safeharbor/app.py
+++ safeharbor/app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	return 'hello, world!'
+
+@app.route('/api/safeharbor', methods=["POST"])
+def safeharbor():
+    json_data = request.get_json()
+    email = json_data['email']
+    password = json_data['password']
+    return jsonify(email, password)
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,3 +4,12 @@ def test_hello():
 	with app.test_client() as client:
 	    response = client.get("/")
 	    assert response.status_code == 200
+
+def test_safeharbor():
+	with app.test_client() as client:
+		response = client.post("api/safeharbor",
+			json={
+				"email": "montana@gmail.com",
+				"password": "123456"
+			})
+		assert response.status_code == 200
