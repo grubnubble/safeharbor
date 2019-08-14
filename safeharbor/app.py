82b949f388847a18154f719d3ec521e4dfef28db
grubnubble <montanalisagoodman@gmail.com>
2019-08-14 14:39:15 -0400

    Use a class for patient data
    - adds .gitignore

--- safeharbor/app.py
+++ safeharbor/app.py
@@ -1,6 +1,27 @@
+import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+class Patient(object):
+	def __init__(self, birthDate, zipCode, admissionDate, dischargeDate, notes):
+		self.birthDate = birthDate
+		self.zipCode = zipCode
+		self.admissionDate = admissionDate
+		self.dischargeDate = dischargeDate
+		self.notes = notes
+
+	@staticmethod
+	def from_json(data):
+		return Patient(
+			data["birthDate"],
+			data["zipCode"],
+			data["admissionDate"],
+			data["dischargeDate"],
+			data["notes"])
+
+	def to_json(self):
+		return json.dumps(self.__dict__)
 
 @app.route('/')
 def hello():
@@ -9,6 +30,5 @@ def hello():
 @app.route('/api/safeharbor', methods=["POST"])
 def safeharbor():
     json_data = request.get_json()
-    email = json_data['email']
-    password = json_data['password']
-    return jsonify(email, password)
+    patient = Patient.from_json(json_data)
+    return patient.to_json()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,14 @@
+import json
 from safeharbor.app import app
 
 client = app.test_client()
+patient_data = {
+	"birthDate": "2000-01-01",
+    "zipCode": "10013",
+    "admissionDate": "2019-03-12",
+    "dischargeDate": "2019-03-14",
+    "notes": "Patient with ssn 123-45-6789 previously presented under different ssn"
+}
 
 def test_hello():
     response = client.get("/")
@@ -8,8 +16,10 @@ def test_hello():
 
 def test_safeharbor():
 	response = client.post("api/safeharbor",
-		json={
-			"email": "montana@gmail.com",
-			"password": "123456"
-		})
+		json=patient_data)
 	assert response.status_code == 200
+	assert json.loads(response.data)["birthDate"] == patient_data["birthDate"]
+	assert json.loads(response.data)["zipCode"] == patient_data["zipCode"]
+	assert json.loads(response.data)["admissionDate"] == patient_data["admissionDate"]
+	assert json.loads(response.data)["dischargeDate"] == patient_data["dischargeDate"]
+	assert json.loads(response.data)["notes"] == patient_data["notes"]
